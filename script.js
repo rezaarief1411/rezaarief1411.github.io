966d3eed20fd21bc511b951caf20176f31574ea9
rezaarief1411 <reza.arief@sirclo.com>
2024-11-18 22:46:14 +0700

    first content

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const header = document.querySelector('header');
+header.addEventListener('mouseover', () => {
+    header.style.backgroundColor = '#e0e0e0';
+});
+header.addEventListener('mouseout', () => {
+    header.style.backgroundColor = '#f2f2f2';
+});
